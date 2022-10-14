0cd886eace4f1b07785e758f2f657a9f7c7e6ebb
giusscos <giu.cosenza08@gmail.com>
2022-10-14 15:37:37 +0200

    Fix migration - Add seed by config

--- database/migrations/2022_10_14_132500_create_comics_table.php
+++ database/migrations/2022_10_14_132500_create_comics_table.php
@@ -16,7 +16,7 @@ class CreateComicsTable extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('thumb');
             $table->smallInteger('price');
             $table->string('series');
--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -12,6 +12,18 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        $comics = config('comics');
+
+        foreach($comics as $comic){
+            $c = new Comic();
+            $c->title = $comic['title'];
+            $c->description = $comic['description'];
+            $c->thumb = $comic['thumb'];
+            $c->price = $comic['price'];
+            $c->series = $comic['series'];
+            $c->sale_date = $comic['sale_date'];
+            $c->type = $comic['type'];
+            $c->save();
+        }
     }
 }
